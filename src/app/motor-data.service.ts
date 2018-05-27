f6a3b1c2a615b5341be3d9cc659f148af7128190
LucasCarvalho95 <38766603+LucasCarvalho95@users.noreply.github.com>
2018-05-27 19:04:03 -0300

    moving back project to master

---
+++ src/app/motor-data.service.ts
@@ -0,0 +1,50 @@
+  import {Injectable} from '@angular/core';
+  import {Motor} from './motor';
+  import { HttpClient } from '@angular/common/http';
+
+  @Injectable({
+    providedIn: 'root',
+  })
+  export class MotorDataService {
+
+  lastId: number = 0;
+
+  motors: Motor[] = [];
+
+  constructor(private httpClient : HttpClient) {
+  }
+
+  addMotor(motor: Motor): MotorDataService {
+    if (!motor.id) {
+      motor.id = ++this.lastId;
+    }
+    this.motors.push(motor);
+    return this;
+  }
+
+  deleteMotorById(id: number): MotorDataService {
+    this.motors = this.motors
+      .filter(motor => motor.id !== id);
+    return this;
+  }
+
+  updateMotorById(id: number, values: Object = {}): Motor {
+    let motor = this.getMotorById(id);
+    if (!Motor) {
+      return null;
+    }
+    Object.assign(motor, values);
+    return motor;
+  }
+
+  getAllMotors(): Motor[] {
+    return this.motors;
+  }
+
+  getMotorById(id: number): Motor {
+    return this.motors
+      .filter(motor => motor.id === id)
+      .pop();
+  }
+
+}
